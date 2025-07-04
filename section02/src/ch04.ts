5895f00382d278af810e4aa943f71c79785d94df
yshls <shluxnsal01@gmail.com>
2025-07-05 01:33:33 +0900

    타입 별칭

---
+++ section02/src/ch04.ts
@@ -0,0 +1,27 @@
+// 타입 별칭
+
+type User = {
+  id: number;
+  name: string;
+  nickname: string;
+  birthday: string;
+  bio: string;
+  locaioin: string;
+};
+let user1: User = {
+  id: 1,
+  name: 'alice',
+  nickname: 'moo',
+  birthday: '1990-01-01',
+  bio: 'ALICE IS A SOFTWARE ENGINEER',
+  locaioin: 'seoul',
+};
+
+let user2: User = {
+  id: 1,
+  name: 'alice',
+  nickname: 'moo',
+  birthday: '1990-01-01',
+  bio: 'ALICE IS A SOFTWARE ENGINEER',
+  locaioin: 'seoul',
+};
