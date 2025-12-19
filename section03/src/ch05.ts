7c5fc8ad0f095d243c9d70304e959ec51c9408e5
yshls <shluxnsal01@gmail.com>
2025-12-19 23:49:24 +0900

    타입 추론

---
+++ section03/src/ch05.ts
@@ -0,0 +1,32 @@
+// 타입추론
+
+let a = 10;
+let b = 'hello';
+
+let c = {
+  id: 1,
+  name: '이정환',
+  profile: {
+    nickname: 'winterlood',
+  },
+  urls: ['https://winterlood.com'],
+};
+
+let { id, name, profile } = c;
+let [one, two, three] = [1, 'hello', true];
+
+function func(message = 'hello') {
+  return 'hello';
+}
+
+let d;
+d = 10;
+d.toFixed();
+
+d = 'hello';
+d.toLowerCase();
+
+const num = 10;
+const str = 'hello';
+
+let arr = [1, 'string'];
