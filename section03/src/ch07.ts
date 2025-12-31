b463af1f7c5ebde73a57db962861a827acee87a2
yshls <shluxnsal01@gmail.com>
2025-12-31 23:04:10 +0900

    타입 좁히기

---
+++ section03/src/ch07.ts
@@ -0,0 +1,29 @@
+/**
+타입 좁히기
+조건문 등을 이용해 넓은 타입에서 좁은 타입으로
+타입을 상황에 따라 좁히는 방법을 이야기
+*/
+
+// value => number:toFixed
+// value => string: toUpperCase
+// value => Date: getTime
+type Person = {
+  name: 'string';
+  age: number;
+};
+
+function func(value: number | string | Date | null) {
+  value;
+
+  //   value.toUpperCase();
+  //   value.toFixed();
+  if (typeof value === 'number') {
+    console.log(value.toFixed());
+  } else if (typeof value === 'string') {
+    console.log(value.toUpperCase());
+  } else if (value instanceof Date) {
+    console.log(value.getTime());
+  } else if (value && 'age' in value) {
+    console.log(`${value.name}은 ${value.age}살 입니다`);
+  }
+}
