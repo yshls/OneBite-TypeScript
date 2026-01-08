056a882fe3132f216652e13a331a9f6a3ea0a50f
yshls <shluxnsal01@gmail.com>
2026-01-08 21:04:18 +0900

    함수 타입 표현식

---
+++ section04/src/ch01.ts
@@ -0,0 +1,21 @@
+/**
+함수 타입 표현식
+*/
+
+type Operation = (a: number, b: number) => number;
+
+const add: Operation = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const multipy: (a: number, b: number) => number = (a, b) => a * b;
+
+// 호출시그니처
+type Operation2 = {
+  (a: number, b: number): number;
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multipy2: (a: number, b: number) => number = (a, b) => a * b;
+
+// add2();
+add2.name;
