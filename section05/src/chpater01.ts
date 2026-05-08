16e10ee9fe5b2718bd95ca8e0ecfad4532de3846
yshls <shluxnsal01@gmail.com>
2026-05-08 15:32:35 +0900

    study: 인터페이스 확장

---
+++ section05/src/chpater01.ts
@@ -0,0 +1,26 @@
+/**
+인터페이스의 확장
+*/
+interface Animal {
+  name: string;
+  age: number;
+}
+
+interface Dog extends Animal {
+  isBark: boolean;
+}
+
+interface Cat extends Animal {
+  isScratch: boolean;
+}
+
+interface Chicken extends Animal {
+  isFly: boolean;
+}
+
+interface DogCat extends Dog, Cat {
+  name: '';
+  color: '';
+  isBark: true;
+  isScartch: true;
+}
